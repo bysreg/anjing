ba76a6e19f72df97697a6b689a88100ed9359a55
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2015-07-19 20:35:33 -0400

    working glew

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -1,6 +1,6 @@
 #include "app/app.hpp"
 
-//#include <GL/glew.h>
+#include <GL/glew.h>
 #include <SDL.h>
 
 #include <cassert>
@@ -9,30 +9,46 @@
 using namespace std;
 using namespace anjing;
 
-int App::StartApplication(App* app, int width, int height, int fps, std::string title)
+int App::StartApplication(App* app, int width, int height, int fps, const string& title)
 {
 	assert(app);
 
 	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0){
-		std::cerr << "SDL_Init error: " << SDL_GetError() << std::endl;
+		cerr << "SDL_Init error: " << SDL_GetError() << endl;
 		return -1;
 	}
 
-	/*GLenum err = glewInit();
-	if (err != GLEW_OK)
+	SDL_Window *window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+	if (window == NULL)
 	{
-		MessageBox(0, L"glewInit Failed.", 0, 0);
-	}*/
+		cerr << "SDL_CreateWindow error : " << SDL_GetError() << endl;
+		return -2;
+	}
 
-	app->Init();
+	SDL_GLContext glContext = SDL_GL_CreateContext(window);
+	if (glContext == NULL)
+	{
+		cerr << "SDL_GL_CreateContext failed" << endl;
+		return -3;
+	}
+
+	GLenum glewErr = glewInit();
+	if (glewErr != GLEW_OK)
+	{
+		cerr << "glewInit failed : " << glewGetErrorString(glewErr) << endl;
+		return -4;
+	}
+
+	if (app->Init() == 0)
+	{
+		cout << "application initialized" << endl;
+	}
 
 	return 0;
 }
 
 int App::Init()
 {
-
-
 	return 0;
 }
 
--- src/app/app.hpp
+++ src/app/app.hpp
@@ -7,7 +7,7 @@ namespace anjing
 	class App 
 	{
 	public:
-		static int StartApplication(App* app, int width, int height, int fps, std::string title);
+		static int StartApplication(App* app, int width, int height, int fps, const std::string& title);
 
 		virtual int Init();		
 		virtual void Clean();
