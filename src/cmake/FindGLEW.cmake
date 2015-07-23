ee161d2ccace181521237539efaf9ed4422177e3
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2015-07-22 23:10:58 -0400

    removed unused lines in cmake

--- src/cmake/FindGLEW.cmake
+++ src/cmake/FindGLEW.cmake
@@ -24,9 +24,6 @@ find_path(GLEW_INCLUDE_DIR
 find_library(GLEW_LIBRARY NAMES GLEW glew32 glew glew32s 
 	PATH_SUFFIXES lib64)
 
-#set(GLEW_INCLUDE_DIRS ${GLEW_INCLUDE_DIR})
-#set(GLEW_LIBRARIES ${GLEW_LIBRARY})
-
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 find_package_handle_standard_args(GLEW
                                   REQUIRED_VARS GLEW_INCLUDE_DIR GLEW_LIBRARY)
