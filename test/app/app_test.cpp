7518dc07dc3d96e74f30be49ae27c18d6b7b5e8c
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2015-08-03 01:22:48 -0400

    fixed wrong lib included in windows folder (the previous gtest libs is built with /MT and /MTd) included also the debug library for gtest fixed the libraries linking order in test/app/CMakeLists.txt, made SDL to be the last because of the error "unresolved external symbol _SDL_main". Linking with gtest_main will include the main function for the tests so thats why it must be linked first before SDL

--- test/app/app_test.cpp
+++ test/app/app_test.cpp
@@ -10,15 +10,3 @@ TEST(AppTest, NormalUseCase) {
 	EXPECT_EQ(0, App::StartApplication(&app, 800, 600, 60, "My App"));
 	
 }
-
-// Step 3. Call RUN_ALL_TESTS() in main().
-//
-// We do this by linking in src/gtest_main.cc file, which consists of
-// a main() function which calls RUN_ALL_TESTS() for us.
-//
-// This runs all the tests you've defined, prints the result, and
-// returns 0 if successful, or 1 otherwise.
-//
-// Did you notice that we didn't register the tests?  The
-// RUN_ALL_TESTS() macro magically knows about all the tests we
-// defined.  Isn't this convenient?
