09c468a7074b29cebcdde81834dc87c8d243070a
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2016-01-06 23:18:13 -0500

    Define ANJING_REPLACE_GLOBAL_NEW_DELETE_FILE_LINE in forced_include.hpp when ANJING_DEBUG is defined

--- src/core/forced_include.hpp
+++ src/core/forced_include.hpp
@@ -1,4 +1,8 @@
 #pragma once
 
+#ifdef ANJING_DEBUG
+	#define ANJING_REPLACE_GLOBAL_NEW_DELETE_FILE_LINE
+#endif
+
 #include "core/types.hpp"
 #include "core/memory_operator.hpp"
--- src/myapp/defines.hpp
+++ src/myapp/defines.hpp
@@ -3,9 +3,3 @@
 #ifdef ANJING_DEBUG
 	#define MYAPP_DEBUG
 #endif 
-
-#ifdef MYAPP_DEBUG
-	#ifndef ANJING_REPLACE_GLOBAL_NEW_DELETE_FILE_LINE
-		#define ANJING_REPLACE_GLOBAL_NEW_DELETE_FILE_LINE // replace Anew and Adelete macro with the two parameter __FILE__ and __LINE__ version
-	#endif
-#endif
