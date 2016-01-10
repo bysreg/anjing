55394a69cca925035fc4549daf2e429924d3b2cf
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2016-01-10 16:11:34 -0500

    Setup and teardown function for game_object_test.cpp

--- test/core/game_object_test.cpp
+++ test/core/game_object_test.cpp
@@ -3,30 +3,45 @@
 
 #include <gtest/gtest.h>
 
-using namespace anjing;
-using namespace anjing::core;
-
-TEST(GameObjectTest, ConstructionCheck)
+class GameObjectTest : public ::testing::Test
 {
-	GameObject* go = Anew GameObject;
+protected:
 
+	anjing::core::GameObject* go;
+
+	virtual void SetUp() override
+	{
+		::testing::Test::SetUp();
+
+		go = Anew anjing::core::GameObject;
+	}
+
+	virtual void TearDown() override
+	{
+		::testing::Test::TearDown();
+
+		Adelete(go);
+	}
+};
+
+TEST_F(GameObjectTest, ConstructionCheck)
+{
 	EXPECT_NE(go, nullptr);
 }
 
-TEST(GameObjectTest, AddComponentCheck)
-{
-	GameObject* go = Anew GameObject;
-	Component* component = go->AddComponent<Component>();
+TEST_F(GameObjectTest, AddComponentCheck)
+{	
+	anjing::core::Component* component = go->AddComponent<anjing::core::Component>();
 	
 	EXPECT_NE(component, nullptr);
 	EXPECT_EQ(go, component->GetOwner());
-	EXPECT_NE(go->GetComponent<Component>(), nullptr);
+	EXPECT_NE(go->GetComponent<anjing::core::Component>(), nullptr);
 }
 
-TEST(GameObjectTest, AddMeshRendererCheck)
+TEST_F(GameObjectTest, AddMeshRendererCheck)
 {
-	GameObject* go = Anew GameObject;
-	Component* mesh_renderer = go->AddComponent<anjing::gfx::MeshRenderer>();
+	anjing::core::GameObject* go = Anew anjing::core::GameObject;
+	anjing::core::Component* mesh_renderer = go->AddComponent<anjing::gfx::MeshRenderer>();
 
 	EXPECT_NE(mesh_renderer, nullptr);
 	EXPECT_EQ(mesh_renderer->GetOwner(), go);
