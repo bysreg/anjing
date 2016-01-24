26199569bf987668b6cf589a2aa685d134ce0e12
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2016-01-23 20:43:27 -0500

    Fix compile error on linux

--- src/gfx/shader.cpp
+++ src/gfx/shader.cpp
@@ -79,7 +79,8 @@ int anjing::gfx::Shader::CompileShader(GLuint& out_shader, const std::string& fi
 		return 2;
 	}
 
-	glShaderSource(shader, 1, &static_cast<GLchar*>(source), NULL);
+	GLchar* shader_source = static_cast<GLchar*>(source);
+	glShaderSource(shader, 1, &shader_source, NULL);
 	AdeleteArr(source);
 
 	glCompileShader(shader);
