dd183f8ae66bae215f2d617aed26f75567c79539
Muhammad Hilman Beyri <hilmanbeyri@gmail.com>
2017-01-14 14:31:22 -0500

    api change to transform

--- src/core/transform.cpp
+++ src/core/transform.cpp
@@ -1,6 +1,11 @@
 #include "transform.hpp"
 #include "core/game_object.hpp"
 
+anjing::core::Transform::Transform()
+{
+	mat.SetAsIdentity();
+}
+
 void anjing::core::Transform::OnAdded()
 {
 	GetOwner()->SetTransform(this);
--- src/core/transform.hpp
+++ src/core/transform.hpp
@@ -2,6 +2,7 @@
 
 #include "core/component.hpp"
 #include "math/tvec3.hpp"
+#include "math/tmat4.hpp"
 
 namespace anjing
 {
@@ -12,17 +13,19 @@ namespace anjing
 
 		public:
 			
-			///
+			/// \brief Default constructor for Transform
+			Transform();
+
 			/// \brief Returns current world position
-			///
-			inline anjing::math::Vec3f GetWorldPosition() const
-			{
-				return wpos;
-			}			
+			anjing::math::Vec3f GetWorldPosition() const { return wpos; }
+
+			/// \brief Returns current transform matrix
+			anjing::math::Mat4f GetMat() const { return mat; }
 
 		private:
 			
 			anjing::math::Vec3f wpos;
+			anjing::math::Mat4f mat;
 
 			virtual void OnAdded() override;
 
